6e552a1f649a3265e91d4ed683aa8a0a1982724e
Michael Hoffman <michaelmhoffman@users.noreply.github.com>
2021-08-31 14:33:06 -0400

    build: change `pdfcomments` to `pdfuris`

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
+"""Setup `pdfuris`."""
+
 from setuptools import setup
 
-setup(name='pdfcomments',
+setup(name='pdfuris',
       version='0.1',
-      description='extract comments from PDF',
+      description='extract URIs from PDF',
       author='Michael Hoffman',
       author_email='michael.hoffman@utoronto.ca',
       license='GPLv3',
-      package_data={"pdfcomments": ["py.typed"]},
-      packages=['pdfcomments'],
+      package_data={"pdfuris": ["py.typed"]},
+      packages=['pdfuris'],
       install_requires=['PyPDF2'],
       python_requires='>=3.6',
       entry_points={
-        'console_scripts': ['pdfcomments=pdfcomments.__main__:main'],
+        'console_scripts': ['pdfuris=pdfuris.__main__:main'],
       },
       zip_safe=False)
