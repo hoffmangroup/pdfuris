cfb70e0c8db6634e9072048ca026e66c526e831f
Michael Hoffman <michaelmhoffman@users.noreply.github.com>
2021-08-31 14:31:52 -0400

    feat: process URIs

--- pdfuris/__main__.py
+++ pdfuris/__main__.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
-"""__main__.py: extract comments from PDF
-"""
+"""__main__.py: Extract URIs from PDF."""
 
 __version__ = "0.1"
 
-# Copyright 2018-2020 Michael M. Hoffman <michael.hoffman@utoronto.ca>
+# Copyright 2018-2021 Michael M. Hoffman <michael.hoffman@utoronto.ca>
 
 from argparse import Namespace
-from collections import defaultdict
-from os import extsep
-from pathlib import Path
-import re
 import sys
-from typing import DefaultDict, Iterator, List, Optional, TextIO
+from typing import Iterator, List
 
 from PyPDF2 import PdfFileReader
 from PyPDF2.pdf import PageObject
@@ -33,27 +28,12 @@ PDF_DOC_ENCODING[13] = "\u000d"
 
 generic._pdfDocEncoding = tuple(PDF_DOC_ENCODING)
 
-# key: int (number of stars)
-# value: list of strs
-SeverityDict = DefaultDict[int, List[str]]
-
 ENCODING = "utf-8"
 
-OUT_EXT = "txt"
 STRICT = False
 
-SEVERITY_NAMES = {0: "Minor comments",
-                  1: "Major comments"}
 
-re_stars = re.compile(
-    r"""^
-    (?P<stars>\**)
-    \s*
-    (?P<comment>.*)
-    $""", re.DOTALL | re.VERBOSE)
-
-
-def iter_annot_contents(page: PageObject) -> Iterator[str]:
+def iter_annot_uris(page: PageObject) -> Iterator[str]:
     try:
         annot_indirects = page["/Annots"]
     except KeyError:
@@ -62,57 +42,36 @@ def iter_annot_contents(page: PageObject) -> Iterator[str]:
     for annot_indirect in annot_indirects:
         annot = annot_indirect.getObject()
 
+        # PDF 1.7 s12.5.6.5 Link Annotations
+        if annot["/Subtype"] != "/Link":
+            continue
+
+        # PDF 1.7 s12.6 Actions
         try:
-            yield annot["/Contents"]
+            action = annot["/A"]
         except KeyError:
             continue
 
+        if action["/S"] == "/URI":
+            yield action["/URI"]
 
-def load_comments(filename: str) -> SeverityDict:
-    res: SeverityDict = defaultdict(list)
 
+def load_uris(filename: str) -> Iterator[str]:
     reader = PdfFileReader(filename, STRICT)
-    for page_num, page in enumerate(reader.pages, 1):
-        for contents in iter_annot_contents(page):
-            m_stars = re_stars.match(contents)
-            assert m_stars is not None  # should always match
-
-            stars = m_stars["stars"]
-            comment = m_stars["comment"]
-
-            # number of stars
-            severity = len(stars)
-
-            res[severity].append(f"p{page_num}: {comment}")
-
-    return res
+    for page in reader.pages:
+        for uri in iter_annot_uris(page):
+            yield uri
 
 
-def get_severity_name(severity: int) -> str:
-    return SEVERITY_NAMES.get(severity, f"Comments, severity {severity}")
+def write_uris(uris: Iterator[str]) -> None:
+    for uri in uris:
+        print(uri)
 
 
-def write_comments(severity: int, comments: List[str], file: TextIO) -> None:
-    print(get_severity_name(severity), ":", sep="", file=file)
+def pdfuris(infilename: str) -> int:
+    uris = load_uris(infilename)
 
-    print(file=file)
-    print(*comments, sep="\n", file=file)
-    print(file=file)
-
-
-def save_comments(severities: SeverityDict, filename: str) -> None:
-    with open(filename, "w") as file:
-        for severity, comments in sorted(severities.items(), reverse=True):
-            write_comments(severity, comments, file)
-
-
-def pdfcomments(infilename: str, outfilename: Optional[str] = None) -> int:
-    severities = load_comments(infilename)
-
-    if outfilename is None:
-        outfilename = extsep.join([Path(infilename).stem, OUT_EXT])
-
-    save_comments(severities, outfilename)
+    write_uris(uris)
 
     return EX_OK
 
@@ -123,9 +82,6 @@ def parse_args(args: List[str]) -> Namespace:
     description = __doc__.splitlines()[0].partition(": ")[2]
     parser = ArgumentParser(description=description)
     parser.add_argument("infile", help="input PDF file")
-    parser.add_argument("outfile", nargs="?",
-                        help="output text file"
-                        " (default: infile with extension changed to 'txt')")
 
     version = f"%(prog)s {__version__}"
     parser.add_argument("--version", action="version", version=version)
@@ -136,7 +92,7 @@ def parse_args(args: List[str]) -> Namespace:
 def main(argv: List[str] = sys.argv[1:]) -> int:
     args = parse_args(argv)
 
-    return pdfcomments(args.infile, args.outfile)
+    return pdfuris(args.infile)
 
 
 if __name__ == "__main__":
